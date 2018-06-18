95d29afb72388d20c0e352044cca6e683084db1b
Thomas Miller <tommydrum777@gmail.com>
2018-06-18 14:22:31 -0700

    Alpha version of bot with ability to add/remove users to requested role

---
+++ Makoto/Commands/RoleManager.cs
@@ -0,0 +1,89 @@
+﻿using Discord.Commands;
+using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace Makoto.Commands
+{
+    public class RoleManager : ModuleBase
+    {
+        #region notify
+        [Command("notify"), Summary("Add's or remove's mentioning user to a notify role")]
+        public async Task Notify([Remainder, Summary("The role name to add/remove yourself into (minus the prefix)")] string args)
+        {
+            //Test if PM, ignore if it is.
+            if (Context.Guild == null)
+            {
+                await ReplyAsync("Please use this in the server you wish to change your role in");
+            }
+            string[] splitargs = args.Split(' ');
+            if (splitargs.Length < 1)
+            {
+                await ReplyAsync("Need's role name, use format: @me notify role");
+            }
+            else
+            {
+                //search for requested role, null if not found
+                var role = Context.Guild.Roles.Where(e => e.Name == "ns_" + splitargs[0]).DefaultIfEmpty(null).First();
+                if (role == null)
+                {
+                    await ReplyAsync("Did not find that role. If you want this to become a role, ask your server admin to create it.");
+                    return;
+                }
+
+                // See if user has the role. If yes, remove the role, of not, add the role.
+                if ((Context.User as IGuildUser).RoleIds.Contains(role.Id))
+                {
+                    var user = await Context.Guild.GetUserAsync(Context.User.Id);
+                    await user.RemoveRoleAsync(role);
+                    await ReplyAsync("You are no longer apart of " + role.Name);
+                }
+                else
+                {
+                    var user = await Context.Guild.GetUserAsync(Context.User.Id);
+                    await user.AddRoleAsync(role);
+                    await ReplyAsync("You are now apart of " + role.Name);
+                }
+            }
+        }
+        #endregion
+
+        #region new
+        [Command("new"), Summary("Create's a new role. Admin only")]
+        [RequireUserPermission(ChannelPermission.ManageRoles)]
+        public async Task NewRole([Remainder, Summary("The role name to create (minus the prefix)")] string args)
+        {
+            //Test if PM, ignore if it is.
+            if (Context.Guild == null)
+            {
+                await ReplyAsync("Please use this in the server you wish to change your role in");
+            }
+            string[] splitargs = args.Split(' ');
+            if (splitargs.Length < 1)
+            {
+                await ReplyAsync("Need's role name, use format: @me notify role");
+            }
+            else
+            {
+                var role = Context.Guild.Roles.Where(e => e.Name == "ns_" + splitargs[0]).DefaultIfEmpty(null).First();
+                if (role == null)
+                {
+                    //Create the role
+                    IRole newRole = await Context.Guild.CreateRoleAsync("ns_" + splitargs[0], GuildPermissions.None, new Color(255, 0, 0), false);
+                    await newRole.ModifyAsync(e => e.Mentionable = true);
+                    await ReplyAsync("Created " + newRole.Name);
+                }
+                else
+                {
+                    await ReplyAsync("Cannot add a role that already exists");
+                    return;
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ Makoto/Program.cs
@@ -0,0 +1,150 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord.Net.Providers.WS4Net;
+using Makoto.Commands;
+
+namespace Makoto
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+
+        // Keep the CommandService and IServiceCollection around for use with commands.
+        private readonly IServiceCollection _map = new ServiceCollection();
+        private readonly CommandService _commands = new CommandService();
+        public IConfigurationRoot Configuration { get; set; }
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            //Configuration file management:
+            var builder = new ConfigurationBuilder()        // Create a new instance of the config builder
+                .SetBasePath(AppContext.BaseDirectory)      // Specify the default location for the config file
+                .AddJsonFile("_configuration.json");        // Add this (json encoded) file to the configuration
+            Configuration = builder.Build();                // Build the configuration
+
+            _client = new DiscordSocketClient(
+                new DiscordSocketConfig
+                {
+                    LogLevel = LogSeverity.Info,
+                    WebSocketProvider = WS4NetProvider.Instance,
+                    AlwaysDownloadUsers = true,
+                    HandlerTimeout = null,
+                });
+
+            //Give the client my logger
+            _client.Log += Log;
+
+            // Centralize the logic for commands into a seperate method.
+            await InitCommands();
+
+            string discordToken = Configuration["tokens:discord"];     // Get the discord token from the config file
+            string token = discordToken; // Remember to keep this private!
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            var cc = Console.ForegroundColor;
+            switch (msg.Severity)
+            {
+                case LogSeverity.Critical:
+                case LogSeverity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LogSeverity.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogSeverity.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case LogSeverity.Verbose:
+                case LogSeverity.Debug:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+            }
+            Console.WriteLine($"{DateTime.Now,-19} [{msg.Severity,8}] {msg.Source}: {msg.Message}");
+            Console.ForegroundColor = cc;
+
+            // If you get an error saying 'CompletedTask' doesn't exist,
+            // your project is targeting .NET 4.5.2 or lower. You'll need
+            // to adjust your project's target framework to 4.6 or higher
+            // (instructions for this are easily Googled).
+            // If you *need* to run on .NET 4.5 for compat/other reasons,
+            // the alternative is to 'return Task.Delay(0);' instead.
+            return Task.CompletedTask;
+        }
+
+        private IServiceProvider _services;
+
+        private async Task InitCommands()
+        {
+            // Repeat this for all the service classes
+            // and other dependencies that your commands might need.
+            _map.AddSingleton(Configuration);
+            _map.AddSingleton(_client);
+            _map.AddSingleton(new CommandService(new CommandServiceConfig
+            {
+                LogLevel = LogSeverity.Verbose,
+                DefaultRunMode = RunMode.Async,
+                CaseSensitiveCommands = false
+
+            }));
+
+            // When all your required services are in the collection, build the container.
+            // Tip: There's an overload taking in a 'validateScopes' bool to make sure
+            // you haven't made any mistakes in your dependency graph.
+            _services = _map.BuildServiceProvider();
+
+            // Either search the program and add all Module classes that can be found.
+            // Module classes *must* be marked 'public' or they will be ignored.
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+            // Or add Modules manually if you prefer to be a little more explicit:
+            await _commands.AddModuleAsync<HelpManager>();
+
+            // Subscribe a handler to see if a message invokes a command.
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            // Bail out if it's a System Message.
+            var msg = arg as SocketUserMessage;
+            if (msg == null) return;
+
+            // Create a number to track where the prefix ends and the command begins
+            int pos = 0;
+            // Replace the '!' with whatever character
+            // you want to prefix your commands with.
+            // Uncomment the second half if you also want
+            // commands to be invoked by mentioning the bot instead.
+            if (msg.HasMentionPrefix(_client.CurrentUser, ref pos))
+            {
+                // Create a Command Context.
+                var context = new SocketCommandContext(_client, msg);
+
+                // Execute the command. (result does not indicate a return value, 
+                // rather an object stating if the command executed succesfully).
+                var result = await _commands.ExecuteAsync(context, pos, _services);
+
+                // Uncomment the following lines if you want the bot
+                // to send a message if it failed (not advised for most situations).
+                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                    await msg.Channel.SendMessageAsync(result.ToString());
+            }
+        }
+    }
+}
