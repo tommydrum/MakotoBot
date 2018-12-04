f33774d8862722152479c7ba4e09aa33a74e1ecc
Thomas Miller <tommydrum777@gmail.com>
2018-12-03 19:12:29 -0800

    Fix scripts to account if the application is not currently installed/running

--- scripts/remove_application.sh
+++ scripts/remove_application.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 rm -rf /home/ec2-user/Makoto
+exit 0 # return success even if removed failed.. because remove will only fail if it doesn't exist.
--- scripts/stop_service.sh
+++ scripts/stop_service.sh
@@ -2,3 +2,5 @@
 
 # stop dotnet application
 systemctl stop kestrel-makoto.service
+pkill dotnet # to be sure the service is dead.
+exit 0 # gracefully exit. If service failed to stop, it's dead now.
